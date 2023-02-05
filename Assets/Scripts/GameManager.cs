73fb0963572b66e3c8fe6dbf596df5191cf1c57d
Daniel Tian <thingbluex@gmail.com>
2023-02-05 17:09:37 -0500

    Build

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -32,6 +32,8 @@ public class GameManager : MonoBehaviour
         doorClosedSprite.SetActive(true);
         doorOpenSprite.SetActive(false);
         doorCollider.enabled = true;
+
+        Screen.fullScreenMode = FullScreenMode.Windowed;
     }
 
     private void FixedUpdate()
